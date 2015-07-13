443474036d79a3cafa93f1914024f41b79220096
guillermopous <guillermo.pous@gmail.com>
2015-07-13 15:42:57 +0200

    Realizado punto 2 del eje. obligatorio. Página de creditos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,13 @@ router.get( '/', function ( req, res ) {
     res.render( 'index', { title: 'Quiz' } );
 } );
 
+router.get( '/author', function ( req, res ) {
+    res.render( 'author', {
+        nombre: 'Guillermo Pous Ardid',
+        foto: '/images/author.png'
+    } );
+} );
+
 router.get( "/quizes/question", quizController.question );
 router.get( "/quizes/answer", quizController.answer );
 
